01c93e92db6bc6e1fda78f367e8d4ee7d7e7279f
tft <tft@SHARMA-ROHIT>
2020-09-30 11:02:25 +0530

    Updates

---
+++ precache-manifest.5ab1ef3d302b3dadad07c61cea1a8c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ba809d7bda893cabd360ef4135a90a",
+    "url": "/India-Covid-Stats/index.html"
+  },
+  {
+    "revision": "70a1e1073629232506df",
+    "url": "/India-Covid-Stats/static/css/main.5a11ecd3.chunk.css"
+  },
+  {
+    "revision": "dfa9b730179b6dbb3aaf",
+    "url": "/India-Covid-Stats/static/js/2.9a0fbaf4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/India-Covid-Stats/static/js/2.9a0fbaf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a1e1073629232506df",
+    "url": "/India-Covid-Stats/static/js/main.f7fe7f16.chunk.js"
+  },
+  {
+    "revision": "200b8a4b10b6b279f571",
+    "url": "/India-Covid-Stats/static/js/runtime-main.b17e14b3.js"
+  }
+]);
