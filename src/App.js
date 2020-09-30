dcef2b79b4d4827847a0947445f7416aa65fc52c
tft <tft@SHARMA-ROHIT>
2020-09-30 10:56:01 +0530

    deploy to github

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navigation from "./components/Navigation/Navigation";
 import StateTable from "./components/StateTable/StateTable";
 import StateDataContextProvider from "./Context/StateDataContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import TestingStats from '../src/components/TestingStats/TestingStats';
+import TestingStats from "../src/components/TestingStats/TestingStats";
 import HospitalBedsStats from "./components/HospitalBeds/HospitalBedsStats";
 import Helpline from "./components/Helpline/Helpline";
 import Notifications from "./components/Notifications/Notifications";
@@ -15,11 +15,11 @@ function App() {
         <StateDataContextProvider>
           <Navigation />
           <Switch>
-          <Route path="/testing-stats" exact component={TestingStats}/>
-          <Route path="/hospital-beds" exact component={HospitalBedsStats}/>
-          <Route path = "/helpline-info" exact component={Helpline}/>
-          <Route path="/notifications" exact component= {Notifications}/>
-          <Route path="/" exact component={StateTable}/>
+            <Route path='/testing-stats' exact component={TestingStats} />
+            <Route path='/hospital-beds' exact component={HospitalBedsStats} />
+            <Route path='/helpline-info' exact component={Helpline} />
+            <Route path='/notifications' exact component={Notifications} />
+            <Route path='/' exact component={StateTable} />
           </Switch>
         </StateDataContextProvider>
       </div>
